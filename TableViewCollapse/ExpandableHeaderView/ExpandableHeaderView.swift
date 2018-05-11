6569a244b6e08c7d53bf67cd1128d291b21c3546
davalcato <davalcato@yahoo.com>
2018-05-10 19:10:58 -0700

    first commit

---
+++ TableViewCollapse/ExpandableHeaderView/ExpandableHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpandableHeaderView.swift
+//  TableViewCollapse
+//
+//  Created by Daval Cato on 5/7/18.
+//  Copyright © 2018 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TableViewCollapse/ExpandableHeaderView/Section.swift
@@ -0,0 +1,9 @@
+//
+//  Section.swift
+//  TableViewCollapse
+//
+//  Created by Daval Cato on 5/7/18.
+//  Copyright © 2018 Daval Cato. All rights reserved.
+//
+
+import Foundation
